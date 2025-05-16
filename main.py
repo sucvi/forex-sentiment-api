98b3a51d9d1b9deacb0a7a1d7f0659fbeb3e96c5
Jan Trefil <jan@example.com>
2025-05-16 21:12:36 +0200

    Přidán /sentiment-data endpoint

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from scraper import get_forexfactory_events
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Sentiment API is live!"}
+
+@app.get("/sentiment-data")
+async def sentiment_data(pair: str = Query(..., min_length=6, max_length=7)):
+    base = pair[:3].upper()
+    quote = pair[3:].upper()
+    calendar = await get_forexfactory_events(base, quote)
+    return {
+        "pair": f"{base}/{quote}",
+        "calendar": calendar
+    }
